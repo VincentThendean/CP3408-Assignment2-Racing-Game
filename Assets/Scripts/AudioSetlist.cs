af7caa4ec00bf0b324a39701d1d196e44934dd99
AndrewLim8140 <andrewfeilong.lim@my.jcu.edu.au>
2024-11-21 21:19:24 +0800

    Adding Audio

---
+++ Assets/Scripts/AudioSetlist.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSetlist : MonoBehaviour
+{
+    public string purpose;
+    public AudioClip[] audioLib;
+    public AudioSource audioSource;
+
+    int Iterator;
+    int prevIndex;
+    AudioClip track;
+    double duration;
+    bool fireTrigger;
+    string gameState;
+
+    private List<double> durationLib;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();  
+
+        if (purpose == "BGM"){
+            gameState = "Menu";
+            durationLib = new List<double>();
+            }
+
+        if (purpose == "SFX"){
+            audioSource.Stop();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // For BGM loop
+        
+            if (purpose == "BGM"){
+                if (!audioSource.isPlaying){
+                    if (gameState == "Menu"){
+                        audioSource.clip = audioLib[0];
+                    }
+                    else if (gameState == "End"){
+                        audioSource.clip = audioLib[6];
+                    }
+                    else if (gameState == "Game"){
+                        
+                        if ((Iterator + 1) >= 5){
+                            Iterator = 2;
+                            prevIndex = 4; 
+                        }else{
+                            prevIndex = Iterator;
+                            Iterator += 1;
+                            }
+
+                        track = audioLib[Iterator];
+                        audioSource.clip = track;
+                    }
+                    audioSource.Play();
+                }
+
+                // replace with gamestate flag
+                if (Input.GetButtonDown("Fire2")){
+                    if (gameState == "Menu"){
+                        audioSource.Stop();
+                        gameState = "Game";
+                        Iterator = 1;
+
+                    }else if (gameState == "Game"){
+                        audioSource.Stop();
+                        gameState = "Menu";
+                    }
+                 
+                }
+
+                // Replace with win flag
+                if (Input.GetButtonDown("Fire3")){
+                    audioSource.Stop();
+                    gameState = "End";
+                }
+
+        
+        
+                }
+
+        else if (purpose == "SFX"){
+            // Replace with fire flag
+            if (Input.GetButtonDown("Fire1")){
+                
+                fireTrigger = true;
+                if (!audioSource.isPlaying){
+                    if (fireTrigger){
+                    audioSource.clip = audioLib[0];
+                    audioSource.Play();
+                    fireTrigger = false;
+                    }
+                }
+            }
+
+            // Replace with speed flag
+            if (Input.GetButtonDown("Jump")){
+                if (!audioSource.isPlaying){
+                    audioSource.clip = audioLib[1];
+                    audioSource.Play();
+                    
+                }
+            }
+            
+        }           
+    }
+}
