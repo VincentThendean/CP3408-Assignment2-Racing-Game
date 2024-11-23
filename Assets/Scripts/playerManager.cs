d6367aaf711ceff42e0ea33ef91e869e1fc5535a
AndrewLim8140 <andrewfeilong.lim@my.jcu.edu.au>
2024-11-23 19:50:05 +0800

    adding player car to scene

---
+++ Assets/Scripts/playerManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerManager : MonoBehaviour
+{
+    public bool isBoosted = false;
+    public bool hasBullets = false;
+    public bool gameStateEnd = false;
+}
